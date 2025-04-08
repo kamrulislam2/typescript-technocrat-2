80aaba4406aa0352306dddcc6db5718f5699435e
Kamrul Islam <kamrulislamabk@gmail.com>
2025-04-08 16:19:20 +0600

    add TypeScript example for Required utility type in utility types section

--- Module2/2.11.ts
+++ Module2/2.11.ts
@@ -14,4 +14,9 @@
   // Omit type or omit utility // mane bad deya
   type ContactInfo = Omit<Person, "name" | "age">; // ekhane name and age bad deya hoiche
   // tahole contactInfo te amra pacchi shudhu contactNo and email
+
+  // Required type or required utility // mane shobgulo ke required kora
+  type PersonRequired = Required<Person>; // ekhane shobgulo ke required kora hoiche
+
+  
 }
