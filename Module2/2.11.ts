459011020e73afdc09114395706cbd55ab9fcac4
Kamrul Islam <kamrulislamabk@gmail.com>
2025-04-08 16:18:18 +0600

    refactor comments in utility types example for clarity

--- Module2/2.11.ts
+++ Module2/2.11.ts
@@ -1,7 +1,6 @@
 {
   // Utility types
-  // Pick type or pick utility
-
+  // Pick type or pick utility // mane tule ana
   type Person = {
     name: string;
     age: number;
@@ -11,4 +10,8 @@
 
   type Name = Pick<Person, "name">;
   type NameAge = Pick<Person, "name" | "age">;
+
+  // Omit type or omit utility // mane bad deya
+  type ContactInfo = Omit<Person, "name" | "age">; // ekhane name and age bad deya hoiche
+  // tahole contactInfo te amra pacchi shudhu contactNo and email
 }
